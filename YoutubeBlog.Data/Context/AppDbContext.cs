d609aad7e75f0ef240d070f1dc8ed6ea952ed1ec
Kocabay <Kocabay1965.>
2025-03-07 17:32:28 +0300

    dbContext

---
+++ YoutubeBlog.Data/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YoutubeBlog.Entity.Entities;
+
+namespace YoutubeBlog.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext()
+        {
+
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Article> Articles{ get; set; } 
+        public DbSet<Category> Categories { get; set; } 
+        public DbSet<Image> Images { get; set; } 
+    }
+}
--- YoutubeBlog.Web/Program.cs
+++ YoutubeBlog.Web/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using YoutubeBlog.Data.Context;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+//apsetting ile veritabaný oluþturma. ef core ile veritabaný oluþturma
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
